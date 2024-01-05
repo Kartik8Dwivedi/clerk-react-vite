be974367d36d51082c470f1f131b80e2bd3b2759
Kartik8Dwivedi <codewithme.kartik@gmail.com>
2024-01-06 04:29:47 +0530

    page routing setup done login/signup working

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,42 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { ClerkProvider } from '@clerk/clerk-react'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUNISHABLE_KEY
+// Import the layouts
+import RootLayout from "./layouts/root-layout";
+import DashboardLayout from "./layouts/dashboard-layout";
 
-console.log(PUBLISHABLE_KEY)
+// Import the components
+import IndexPage from "./routes";
+import ContactPage from "./routes/contact";
+import SignInPage from "./routes/sign-in";
+import SignUpPage from "./routes/sign-up";
+import DashboardPage from "./routes/dashboard";
+import InvoicesPage from "./routes/invoices";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <ClerkProvider publishableKey={PUBLISHABLE_KEY} >
-    <App />
-  </ClerkProvider>
-)
+const router = createBrowserRouter([
+  {
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <IndexPage /> },
+      { path: "/contact", element: <ContactPage /> },
+      { path: "/sign-in", element: <SignInPage /> },
+      { path: "/sign-up", element: <SignUpPage /> },
+      {
+        element: <DashboardLayout />,
+        path: "dashboard",
+        children: [
+          { path: "/dashboard", element: <DashboardPage /> },
+          { path: "/dashboard/invoices", element: <InvoicesPage /> },
+        ],
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
