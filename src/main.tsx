fe0f118a18b31cbdd2e6d0d806a30068b469722f
Kartik8Dwivedi <codewithme.kartik@gmail.com>
2024-01-06 04:16:52 +0530

    env setup done, along with signup/signin button/views updated

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { ClerkProvider } from '@clerk/clerk-react'
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUNISHABLE_KEY
+
+console.log(PUBLISHABLE_KEY)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY} >
     <App />
-  </React.StrictMode>,
+  </ClerkProvider>
 )
